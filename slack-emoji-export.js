b8cb52ece89226ce167b47c0da6273979e6e70b9
kazuya-nakahara <kazuya.nakahara@bizreach.co.jp>
2018-10-26 22:48:41 +0900

    Update slack-emoji-export.js

---
+++ slack-emoji-export.js
@@ -0,0 +1,66 @@
+'use strict';
+const Slack = require('slack-node');
+const request = require('request');
+const fs = require('fs');
+
+// 最初にimageフォルダ作成
+try {
+  fs.mkdirSync('image');
+} catch (e) {
+  // ignore
+}
+
+const apiToken = 'xoxp-170644065895-281812649813-281037470320-90ae66e9a99464e5d1a692a3d05c0814'; // ここにAPIトークンを貼り付ける。
+const slack = new Slack(apiToken);
+const intervalMillisec = 200;
+
+slack.api('emoji.list', (err, response) => {
+  const emoji = response.emoji;
+  const keys = [];
+  for (let k in emoji) {
+    keys.push(k);
+  }
+  let index = 0;
+
+  function storeNextEmoji() {
+    const key = keys[index];
+    index++;
+
+    // keyがなければ終了
+    if (!key) {
+      console.log('Finished.');
+      return;
+    }
+
+    const url = emoji[key];
+
+    // 存在しない場合やエイリアスは無視
+    if (!url || url.match(/alias/)) {
+      storeNextEmoji();
+      return;
+    }
+
+    console.log('Downloading:');
+    console.log(key);
+    console.log(url);
+
+    const extention = url.match(/\.[^\.]+$/);
+
+    request
+      .get(url)
+      .on('response', response => {
+        console.log('StatusCode:');
+        console.log(response.statusCode);
+        setTimeout(storeNextEmoji, intervalMillisec);
+      })
+      .on('error', error => {
+        if (error) {
+          console.log('Error:');
+          console.log(error);
+        }
+      })
+      .pipe(fs.createWriteStream('image/' + key + extention));
+  }
+
+  storeNextEmoji();
+});
